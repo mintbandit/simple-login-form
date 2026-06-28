aecd5807038f5de140ef7a42f172047c30bf7cc8
Patrick Murray <113388917+mintbandit@users.noreply.github.com>
2026-06-28 15:38:11 +0100

    Added username and password rules to sign in page

--- src/frontend/signin.ts
+++ src/frontend/signin.ts
@@ -1,4 +1,6 @@
 import { FieldError } from "./field-error";
+import { checkComplexity } from "../shared/password-rules";
+import { checkUsername } from "../shared/username-rules";
 
 const passwordField = document.getElementById('password') as HTMLInputElement;
 const passwordInvalidLabel =  document.getElementById('invalid-password') as HTMLInputElement;
@@ -11,7 +13,7 @@ const submitBtm = document.getElementById('form-submit');
 const errors = new FieldError();
 
 function updateSubmitBtn(): void {
-  if(errors.isEmpty() && emailField.value.length && passwordField.value.length){
+  if(errors.isEmpty() && emailField.value.length > 0 && passwordField.value.length > 0){
     submitBtm?.classList.remove('btn-disabled');
   } else {
     submitBtm?.classList.add('btn-disabled');
@@ -19,9 +21,23 @@ function updateSubmitBtn(): void {
 }
 
 emailField.addEventListener('input', (_) =>{
+  const userNameFailures = checkUsername(emailField.value);
+  if(userNameFailures.length > 0){
+    const formattedErrors = userNameFailures.join('<br>');
+    errors.set('invalid-email', emailField, emailInvalidLabel, formattedErrors);
+  } else {
+    errors.remove('invalid-email', emailField, emailInvalidLabel)
+  }
   updateSubmitBtn();
 });
 
 passwordField.addEventListener('input', (_) =>{
+  const passwordFailures = checkComplexity(passwordField.value);
+  if(passwordFailures.length > 0){
+    const formattedErrors = passwordFailures.join('<br>');
+    errors.set('invalid-password', passwordField, passwordInvalidLabel, formattedErrors);
+  } else {
+    errors.remove('invalid-password', passwordField, passwordInvalidLabel);
+  }
   updateSubmitBtn();
 });
---
+++ src/shared/password-rules.ts
@@ -0,0 +1,9 @@
+export function checkComplexity(plainPassword: string): string[]{
+  const errors: string[] = [];
+  if(plainPassword.length < 8){
+    errors.push('Password must be at least 8 characters long.')
+  }
+
+  // Add more rules if applicable
+  return errors;
+}
