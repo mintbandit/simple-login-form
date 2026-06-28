3127a55a0e9bca91a1df6c8faaebfb10b2440610
Patrick Murray <113388917+mintbandit@users.noreply.github.com>
2026-06-28 15:44:19 +0100

    Added username and password rules to sign up page

---
+++ src/frontend/signup.ts
@@ -0,0 +1,51 @@
+import { FieldError } from "./field-error";
+import { checkComplexity } from "../shared/password-rules";
+import { checkUsername } from "../shared/username-rules";
+
+const passwordField = document.getElementById('password') as HTMLInputElement;
+const passwordInvalidLabel =  document.getElementById('invalid-password') as HTMLInputElement;
+
+const emailField = document.getElementById('email') as HTMLInputElement;
+const emailInvalidLabel =  document.getElementById('invalid-email') as HTMLInputElement;
+
+const agreeTosCheckBox = document.getElementById('agree-tos') as HTMLInputElement;
+
+const submitBtm = document.getElementById('form-submit');
+
+const errors = new FieldError();
+
+function updateSubmitBtn(): void {
+  if(errors.isEmpty() && emailField.value.length > 0 && passwordField.value > 0 && agreeTosCheckBox.checked){
+    submitBtm?.classList.remove('btn-disabled');
+  } else {
+    submitBtm?.classList.add('btn-disabled');
+  }
+}
+
+emailField.addEventListener('input', (_) =>{
+  const userNameFailures = checkUsername(emailField.value);
+  if(userNameFailures.length > 0){
+    const formattedErrors = userNameFailures.join('<br>');
+    errors.set('invalid-email', emailField, emailInvalidLabel, formattedErrors);
+  } else {
+    errors.remove('invalid-email', emailField, emailInvalidLabel)
+  }
+  updateSubmitBtn();
+})
+
+agreeTosCheckBox.addEventListener('change', (_) =>{
+  updateSubmitBtn();
+})
+
+passwordField.addEventListener('input', (_) =>{
+  const passwordFailures = checkComplexity(passwordField.value);
+  if(passwordFailures.length > 0){
+    const formattedErrors = passwordFailures.join('<br>');
+    errors.set('invalid-password', passwordField, passwordInvalidLabel, formattedErrors);
+  } else {
+    errors.remove('invalid-password', passwordField, passwordInvalidLabel);
+  }
+  updateSubmitBtn();
+});
+
+agreeTosCheckBox.checked = false;
